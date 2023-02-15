0949a871f4f61fd8a2f6e503c5247fb59dded7ef
cgariette <clifftonafande@gmail.com>
2023-02-15 15:07:38 +0300

    calculator update

--- calculator.c
+++ calculator.c
@@ -12,10 +12,28 @@ int main(){
 		char operator;
 		
 		printf("Please enter your arithmetic operator :");
-		scanf("%d", &operator);
+		scanf("%hhd", &operator);
 		
 		if(operator='+'){
 			result=x+y;
-			printf("The sum of the two numbers is %d ", &result);
+			printf("The sum of the two numbers is %d ", result);
 		}
+		else if(operator='-'){
+			result=x-y;
+			printf("Subtracting the two numbers results to %d ", result);
+		}
+		else if(operator='*'){
+			result=x*y;
+			printf("Multiplying the two numbers results to %d ", result);
+		}
+		else if(operator='/'){
+			result=x/y;
+			printf("Dividing the two numbers results to %d ", result);
+		}
+		else if(operator='%'){
+			result=x%y;
+			printf("The modulus of the two numbers is %d ", result);
+		}
+
+		return 0;
 }
