1c460e8a7fafac5ede5a9eec0ae6783a0970eff4
cgariette <clifftonafande@gmail.com>
2023-03-21 15:03:53 +0300

    calc

--- calculator.c
+++ calculator.c
@@ -1,9 +1,41 @@
 #include <stdio.h>
 
 int main(){
-		int x, y, result;
-		
-		printf("Please enter your first number: ");
+		int x, y, sum, div, mul, sub, mod;
+		char operator;
+
+
+		printf("Enter operator");
+		scanf("%c", &operator);
+		printf("Enter two operands: ");
+		scanf("%d %d", &x, &y);
+
+		switch(operator)
+		{
+			case '+':
+				sum = x + y;
+				printf("sum = %d", sum);
+				break;
+			case '-':
+				sub = x- y;
+				printf("subtraction = %d", sub);
+				break;
+			case '*':
+				mul = x * y;
+				printf("Multiplication = %d", mul);
+				break;
+			case '/':
+				div = x / y;
+				printf("Division = %d", div);
+				break;
+			case '%':
+				mod = x % y;
+				printf("Modulus = %d", mod);
+				break;
+
+		}
+
+		/**printf("Please enter your first number: ");
 		scanf("%d", &x);
 
 		printf("Please enter your second number: ");
@@ -33,7 +65,7 @@ int main(){
 		else if(operator='%'){
 			result=x%y;
 			printf("The modulus of the two numbers is %d ", result);
-		}
+		}*/
 
 		return 0;
 }
